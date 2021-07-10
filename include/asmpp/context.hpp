53be09db61da3418208fa751d6de400246947d87
kcwl <767899122@qq.com>
2021-07-10 10:11:33 +0800

    update 模版参数func未引用

--- include/asmpp/context.hpp
+++ include/asmpp/context.hpp
@@ -68,7 +68,7 @@ namespace asmpp
 			ctx_update{pool_.get_service()}.query<T>(value,condition,std::forward<Func>(f));
 		}
 
-		template<typename T, typename Func>
+		template<typename T>
 		void update(const T& value, const std::string& condition="")
 		{
 			ctx_update{pool_.get_service()}.query<T>(value, condition, [](auto ec){});
